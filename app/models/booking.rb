8a3566ab44fb9bdc1f8f219c61c13f439e93d937
Yung Cher Ho <ycho527@hotmail.com>
2020-03-24 14:14:08 +0800

    Fixed login issue

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,12 @@
 class Booking < ApplicationRecord
   belongs_to :user
+
 end
+
+
+    # t.date "date"
+    # t.integer "start_time"
+    # t.integer "duration"
+    # t.string "special_requests"
+    # t.integer "total_credits"
+    # t.integer "remaining_credits"
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,5 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :owner?, presence: true
 
 end
