8be7df8cd82a7044951130841e060b8c7dab0b6f
Yung Cher Ho <ycho527@hotmail.com>
2020-03-25 17:50:47 +0800

    Saved bookings

--- app/javascript/packs/init_booking.js
+++ app/javascript/packs/init_booking.js
@@ -55,53 +55,17 @@ const removeAllActive = () => {
 
 
 
-
-
-
-
-// const initBooking = () => {
-//   timeSlots.forEach((timeSlot) => {
-//     if (document.querySelectorAll('.active').length === 0) {
-//       if (!timeSlot.classList.contains('unavailable')) {
-//         timeSlot.addEventListener('click', (event) => {
-//           event.currentTarget.classList.add('active');
-//           displayPrice();
-//         })
-//       }
-//     } else if (document.querySelectorAll('.active').length === 1) {
-//         console.log("here")
-
-//         timeSlot.addEventListener('click', (event) => {
-//           console.log("here")
-//           const schedule = document.querySelector('.time-slots');
-//           event.currentTarget.classList.add('active');
-//           let arr = [];
-//           let schArr = Array.from(schedule.children);
-//           console.log(schArr)
-//           schArr.forEach((element, index) => {
-//             if (element.classList.contains("active")) {
-//               arr.push(index);
-//             }
-//           })
-//           let sliced = schArr.slice(arr[0], arr[1])
-//           sliced.forEach(element => {
-//             element.classList.add('active');
-//           })
-//           console.log(sliced)
-//           displayPrice();
-
-//         })
-
-//   }
-
-
-
-
-
 const displayPrice = () => {
   const activeSlots = Array.from(document.querySelectorAll('.time-slot.active')).length;
   totalPrice.innerText = `Total price: $${ 10 * activeSlots }`;
+  updateValues();
 };
 
+const updateValues = () => {
+  const bookingData = document.getElementById('booking-data');
+  const startTime = document.querySelectorAll('.active')[0].dataset.time;
+  const duration = document.querySelectorAll('.time-slot.active').length;
+  bookingData.value = `${startTime},${duration}`;
+}
 
 export { initBooking };
