054e93f2d93f6c3476abdc3b68b8c31240e80f2e
Yung Cher Ho <ycho527@hotmail.com>
2020-03-26 18:00:50 +0800

    Completed bookmarks

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,16 @@
 class BookmarksController < ApplicationController
-  def new
-  end
   def create
+    cafe = Cafe.find(params[:cafe_id])
+    Bookmark.create(user: current_user, cafe: cafe)
+
+    respond_to do |format|
+      format.json  { render :json => {message: 'OK'} }
+    end
   end
+
   def index
   end
+
   def destroy
   end
 end
