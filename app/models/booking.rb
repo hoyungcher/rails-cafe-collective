0765df8478b6a435547ef2b840bbb07787808b8f
Kevan Koh <kevankohjinwen@gmail.com>
2020-03-26 10:58:24 +0700

    added booking seeds

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,10 @@
 class Booking < ApplicationRecord
   belongs_to :user
 
+  validates :date, presence: true
+  validates :start_time, presence: true, uniqueness: { scope: :date,
+    message: "should not have two multiple bookings on the same day" }
+  validates :duration, presence: true
 end
 
 
