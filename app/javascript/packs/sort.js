3686088242825ed34c561f3b937725cdce8c78d8
Amanda Ng <amandaleahng@gmail.com>
2020-03-26 14:49:07 +0800

    sort by price working

--- app/javascript/packs/sort.js
+++ app/javascript/packs/sort.js
@@ -1,8 +1,24 @@
 const sortRatingBtn = document.querySelector('.sort-rating');
 const sortPriceBtn = document.querySelector('.sort-price');
 const sortDistanceBtn = document.querySelector('.sort-distance');
-const cafeCards = document.querySelectorAll('.card-cafe');
+
+
+const sortByRating = () => {
+  let cafesToSort = document.querySelectorAll('.card-cafe');
+  cafesToSort = Array.prototype.slice.call(cafesToSort, 0);
+  cafesToSort.sort(function(a, b) {
+    let aord = +parseInt(a.dataset.rating);
+    let bord = +parseInt(b.dataset.rating);
+    return bord - aord;
+  })
+  let parent = document.querySelector('.cafe-container');
+  parent.innerHTML = "";
+  for(let i = 0, l = cafesToSort.length; i < l; i++) {
+    parent.appendChild(cafesToSort[i]);
+  }
+}
 
 sortRatingBtn.addEventListener('click', (e) => {
-  console.log("hello")
+  console.log("hello");
+  sortByRating();
 })
