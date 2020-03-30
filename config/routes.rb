a8d723db3a201a26361f71f1cefd5b260cda7c79
Yung Cher Ho <ycho527@hotmail.com>
2020-03-30 17:14:10 +0800

    Added orders controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cafes, only: [ :index, :show ] do
     resources :users, only: [:show] do
-      resources :bookings, only: [:new, :create, :show ]
+      resources :bookings, only: [:new, :create, :show ] do
+        resources :order, only: [:new, :create]
+      end
     end
   end
   resources :users, only: [ :show] do
@@ -12,9 +14,10 @@ Rails.application.routes.draw do
     resources :cafes, only: [ :new, :create, :edit, :update, :destroy ] do
       resources :bookmarks, only: [:create, :destroy]
       resources :hourly_slots, only: [ :new, :create, :destroy ]
+      resources :orders, only: [ :index ]
     end
     resources :bookings, only: [ :show, :index ] do
-      resources :reviews, only: [ :new, :create, ]
+      resources :reviews, only: [ :new, :create ]
     end
   end
 
