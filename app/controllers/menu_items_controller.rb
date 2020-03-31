058b7a1b0a988e0f9bb63222d326e7ca3da476e0
Yung Cher Ho <ycho527@hotmail.com>
2020-03-31 12:58:24 +0800

    added form

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,13 +1,24 @@
 class MenuItemsController < ApplicationController
   def index
+    @user = User.find(params[:user_id])
     @cafe = Cafe.find(params[:cafe_id])
     @menu_items = @cafe.menu_items
     @menu_item = MenuItem.new
   end
 
   def create
+    @user = User.find(params[:user_id])
+    @cafe = Cafe.find(params[:cafe_id])
+    @menu_item = MenuItem.new(menu_item_params)
+    @menu_item.cafe = @cafe
   end
 
   def destroy
+  end
+
+  private
+
+  def menu_item_params
+    params.require(:menu_item).permit(:name, :description, :category, :price)
   end
 end
