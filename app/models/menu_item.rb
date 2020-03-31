436a0b158ec955b9485f9cc285c240e56f10e41e
Amanda Ng <amandaleahng@gmail.com>
2020-03-31 14:30:35 +0800

    split food and drinks

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -4,7 +4,6 @@ class MenuItem < ApplicationRecord
 
   validates :name, presence: true, uniqueness: { scope: :cafe,
     message: "should only have one of each item on your menu" }
-  validates :description, presence: true
   validates :category, presence: true, inclusion: { in: %w(food drink),
     message: "%{value} is not a valid category" }
   validates :price, presence: true
